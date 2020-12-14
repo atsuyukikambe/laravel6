4af1967ca3c3aa39070e42f51d0106277a962083
atsuyukikambe <73223311+atsuyukikambe@users.noreply.github.com>
2020-12-14 16:43:26 +0900

    hamburger-menu

--- backend/laravel/resources/views/layouts/app.blade.php
+++ backend/laravel/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -22,6 +22,9 @@
 
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+
+    <!-- Tailwind CSS -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -30,9 +33,9 @@
             @guest
             <nav id="nav">
                 <ul>
-                    <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                    <li><a href="{{ route('login') }}">{{ __('ログイン') }}</a></li>
                     @if (Route::has('register'))
-                    <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                    <li><a href="{{ route('register') }}">{{ __('新規登録') }}</a></li>
                     @endif
                 </ul>
             </nav>
@@ -45,7 +48,7 @@
             <nav id="nav">
                 <ul>
                     <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+                                            document.getElementById('logout-form').submit();">{{ __('ログアウト') }}</a></li>
                     <li><a href="{{ url('/home') }}">HOME</a></li>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
