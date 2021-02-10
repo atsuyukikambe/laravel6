ebc7bb3877b3b6edcd4591d89d85f4a15e5f7160
ynaka6 <yuuki.nakamura.0828@gmail.com>
2021-02-10 13:00:32 +0800

    カレンダーの日付をクリックしたときのイベントサンプルを作成

--- backend/laravel/resources/js/app.js
+++ backend/laravel/resources/js/app.js
@@ -1,16 +1,24 @@
 import { Calendar } from '@fullcalendar/core';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('calendar');
     if (calendarEl) {
         const calendar = new Calendar(calendarEl, {
-            plugins: [ timeGridPlugin ],
+            plugins: [ timeGridPlugin, interactionPlugin ],
             initialView: 'timeGridDay',
             headerToolbar: {
                 left: 'prev,next today',
                 center: 'title',
                 right: 'timeGridDay'
+            },
+            dateClick: function(info) {
+                alert('Clicked on: ' + info.dateStr);
+                console.log(info);
+            },
+            eventClick: (info) => {
+                console.log(info)
             }
         })
         calendar.render()
