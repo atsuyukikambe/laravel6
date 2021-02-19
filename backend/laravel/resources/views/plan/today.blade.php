9a0de9b92bed79421c2eec15e4ba2e0508f33634
atsuyukikambe <73223311+atsuyukikambe@users.noreply.github.com>
2021-02-19 18:16:11 +0900

    modal component

--- backend/laravel/resources/views/plan/today.blade.php
+++ backend/laravel/resources/views/plan/today.blade.php
@@ -11,9 +11,23 @@
 </div>
 <div id="mask" class="hidden"></div>
 <section id="modal" class="hidden">
-    <p>こんにちは</p>
-    <div class="text-center py-6">
-        <div class="px-4 py-2 rounded-full mx-auto text-blue-700 inline-block bg-gray-900 hover:bg-gray-700" id="close">閉じる</div>
+    <div class=" flex items-center justify-center">
+
+        <form id="form" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h1 class="block text-gray-700 font-bold mb-2 text-xl text-center">CUESTIONARIO DE PREGUNTAS Y ENVÍO POR WHATSAPP</h1>
+
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                    Nombre
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="name" id="name" type="text" placeholder="Ingresa tu nombre" required>
+            </div>
+            <div class="text-center py-6">
+                <div class="px-4 py-2 rounded-full mx-auto text-blue-700 inline-block bg-gray-900 hover:bg-gray-700" id="close">閉じる</div>
+            </div>
+        </form>
+
     </div>
+
 </section>
 @endsection
