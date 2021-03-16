33517a3981348ae8e03353102dff42348e34b3b1
ynaka6 <yuuki.nakamura.0828@gmail.com>
2021-03-16 16:09:31 +0800

    Subjectの処理を実装しました

---
+++ backend/laravel/app/Subject.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Subject extends Model
+{
+    protected $fillable = [
+        'label',
+        'name',
+    ];
+    
+    public function plans(): HasMany
+    {
+        return $this->hasMany(Plan::class);
+    }
+
+    public function getBgColorAttribute(): string
+    {
+        return [
+            'Japanese' => 'bg-red-600',
+            'English' => 'bg-pink-600',
+            'Math' => 'bg-blue-700',
+            'Biology' => 'bg-green-400',
+            'Chemistry' => 'bg-green-400',
+            'Physics' => 'bg-green-400',
+            'Japanese-history' => 'bg-green-400',
+            'World-history' => 'bg-yellow-300',
+            'Geography' => 'bg-yellow-300',
+            'Politics-and-economy' => 'bg-yellow-300',
+            'Ethics' => 'bg-yellow-300',
+        ][$this->label] ?? 'bg-gray-400';
+    }
+}
---
+++ backend/laravel/database/seeds/SubjectsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Subject;
+use Illuminate\Database\Seeder;
+
+class SubjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Subject::insert([
+            ['label' => 'Japanese', 'name' => '国語', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'English', 'name' => '英語', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Math', 'name' => '数学', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Biology', 'name' => '生物学', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Chemistry', 'name' => '化学', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Physics', 'name' => '物理学', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Japanese-history', 'name' => '日本史', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'World-history', 'name' => '世界史', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Geography', 'name' => '地理', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Politics-and-economy', 'name' => '政治・経済', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Ethics', 'name' => '倫理', 'created_at' => now(), 'updated_at' => now()],
+            ['label' => 'Others', 'name' => 'その他', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+        
+    }
+}
